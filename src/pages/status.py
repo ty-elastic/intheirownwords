8a68460fb9afb447dfc82eab014cb606c57c8503
Ty Bekiares <tyrone.bekiares@elastic.co>
2023-08-06 21:02:15 +0000

    updates

--- src/pages/status.py
+++ src/pages/status.py
@@ -12,35 +12,38 @@ st.set_page_config(layout="wide", page_title=APP_NAME)
 st.image('https://images.contentstack.io/v3/assets/bltefdd0b53724fa2ce/blt601c406b0b5af740/620577381692951393fdf8d6/elastic-logo-cluster.svg', width=100)
 st.title(APP_NAME)
 
+if 'authentication_status' not in st.session_state:
+    st.session_state['authentication_status'] = False
+if st.session_state["authentication_status"] != True:
+    st.stop()
+
 def update_status(origin):
-  data = {
-    "origin": [],
-    "title": [],
-    "status": [],
-    "started": [],
-    "duration": []
-  }
-  jobs = job.get_status()
-  for j in jobs:
-      if origin is not ORIGIN_ALL and j["origin"] is not origin:
-         continue
-      data['origin'].append(j['origin'])
-      data['title'].append(j['title'])
-      data['status'].append(j['status'])
-      data['started'].append(j['started'])
-      data['duration'].append(str(j['duration']))
+    data = {
+        "origin": [],
+        "title": [],
+        "status": [],
+        "started": [],
+        "duration": []
+    }
+    jobs = job.get_status()
+    for j in jobs:
+        if origin is not ORIGIN_ALL and j["origin"] is not origin:
+            continue
+        data['origin'].append(j['origin'])
+        data['title'].append(j['title'])
+        data['status'].append(j['status'])
+        data['started'].append(j['started'])
+        data['duration'].append(str(j['duration']))
 
-  df = pd.DataFrame(data)
-  st.table(df)
+    df = pd.DataFrame(data)
+    st.table(df)
 
+with st.form("status_query", clear_on_submit=False):
+    origins = es_clauses.get_origins()
+    origins.insert(0, ORIGIN_ALL)
+    origin = st.selectbox('Source', origins)
+    question_button = st.form_submit_button("Search")
 
-if st.session_state["authentication_status"]:
-
-  with st.form("status_query", clear_on_submit=False):
-      origins = es_clauses.get_origins()
-      origins.insert(0, ORIGIN_ALL)
-      origin = st.selectbox('Source', origins)
-      question_button = st.form_submit_button("Search")
-
-  if question_button:
+if question_button:
     update_status(origin)
+
--- src/pages/upload.py
+++ src/pages/upload.py
@@ -8,22 +8,26 @@ st.set_page_config(layout="wide", page_title=APP_NAME)
 st.image('https://images.contentstack.io/v3/assets/bltefdd0b53724fa2ce/blt601c406b0b5af740/620577381692951393fdf8d6/elastic-logo-cluster.svg', width=100)
 st.title(APP_NAME)
 
-if st.session_state["authentication_status"]:
-    
-  with st.form("upload", clear_on_submit=True):
-      source_url = st.text_input("URL of page video was scrapped from (for reference): ")
-      title = st.text_input("title: ")
-      date = st.date_input("date recorded: ")
-      kind = st.text_input("kind: (e.g., webinar, tutorial)")
-      origin = st.text_input("origin: (company)")
-      enable_slides = st.checkbox("content contains slides", value=True)
-      uploaded_file = st.file_uploader("Choose a file")
-      upload_button = st.form_submit_button("Upload")
+if 'authentication_status' not in st.session_state:
+    st.session_state['authentication_status'] = False
+if st.session_state["authentication_status"] != True:
+    st.stop()
 
-  if upload_button:
+with st.form("upload", clear_on_submit=True):
+    source_url = st.text_input(
+        "URL of page video was scrapped from (for reference): ")
+    title = st.text_input("title: ")
+    date = st.date_input("date recorded: ")
+    kind = st.text_input("kind: (e.g., webinar, tutorial)")
+    origin = st.text_input("origin: (company)")
+    enable_slides = st.checkbox("content contains slides", value=True)
+    uploaded_file = st.file_uploader("Choose a file")
+    upload_button = st.form_submit_button("Upload")
 
-      input = os.path.join(job.INGEST_DIR,uploaded_file.name)
-      with open(input, "wb") as f: 
-        f.write(uploaded_file.getbuffer())         
+if upload_button:
+    input = os.path.join(job.INGEST_DIR, uploaded_file.name)
+    with open(input, "wb") as f:
+        f.write(uploaded_file.getbuffer())
 
-      job.enqueue(input, source_url, title, date, kind, origin, enable_slides)
+    job.enqueue(input, source_url, title, date,
+                kind, origin, enable_slides)
