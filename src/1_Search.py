fc614912cee5cddeffee21a1b158ad31e855f59b
Ty Bekiares <tyrone.bekiares@elastic.co>
2023-08-28 22:38:33 +0000

    ty

--- src/1_Search.py
+++ src/1_Search.py
@@ -63,8 +63,9 @@ if st.session_state["authentication_status"]:
     hcol1, hcol2 = st.columns([0.2, 0.8], gap="medium")
     with hcol1:
         if origin_rec is not None and 'logo_url' in origin_rec:
-            print(BASE_URL + origin_rec['logo_url'])
-            st.image(BASE_URL + origin_rec['logo_url'], use_column_width=True)
+            img = BASE_URL + origin_rec['logo_url']
+            print(f"img={img}")
+            st.image(img, use_column_width=True)
         else:
             st.image('https://images.contentstack.io/v3/assets/bltefdd0b53724fa2ce/blt601c406b0b5af740/620577381692951393fdf8d6/elastic-logo-cluster.svg', use_column_width=True)
     with hcol2:
