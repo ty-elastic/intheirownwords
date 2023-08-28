5a55857126b5759cb26ecb0fef748c32262e2cbe
Ty Bekiares <tyrone.bekiares@elastic.co>
2023-08-28 22:07:44 +0000

    ty

--- src/1_Search.py
+++ src/1_Search.py
@@ -15,8 +15,7 @@ from storage import MediaHandler
 from st_inject_api import CustomRule, init_global_tornado_hook, uninitialize_global_tornado_hook
 
 from streamlit_js_eval import get_page_location
-
-st.write(get_page_location())
+BASE_URL = get_page_location().protocol + "//" + get_page_location().host
 
 api.dummy()
 init_global_tornado_hook([CustomRule("/media/.*", MediaHandler, name="/media")])
@@ -63,7 +62,7 @@ if st.session_state["authentication_status"]:
     hcol1, hcol2 = st.columns([0.2, 0.8], gap="medium")
     with hcol1:
         if origin_rec is not None and 'logo_url' in origin_rec:
-            st.image(origin_rec['logo_url'], use_column_width=True)
+            st.image(BASE_URL + origin_rec['logo_url'], use_column_width=True)
         else:
             st.image('https://images.contentstack.io/v3/assets/bltefdd0b53724fa2ce/blt601c406b0b5af740/620577381692951393fdf8d6/elastic-logo-cluster.svg', use_column_width=True)
     with hcol2:
