30d3fa235b8c6d2cdcfdd3fff9c6ab69a807d387
Ty Bekiares <tyrone.bekiares@elastic.co>
2023-08-30 01:22:09 +0000

    y

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -40,7 +40,21 @@ if [[ $(which docker) && $(docker --version) ]]; then
   echo "docker is installed."
 else
   echo "installing docker..."
-  curl https://get.docker.com | sh
+  sudo apt-get update \
+    && sudo apt-get install ca-certificates curl gnupg
+
+  sudo install -m 0755 -d /etc/apt/keyrings \
+    && curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg \
+    && sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+  echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  sudo apt-get update \
+    && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
   sudo systemctl --now enable docker
   sudo usermod -aG docker $USER
 fi
